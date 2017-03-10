cbb2cddd101a1de372302de6f9c606a282f5275a
NA\AIUD457 <aiud457@amway.com>
2017-03-10 15:28:34 -0500

    Initial file structure for project

---
+++ commloop/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+
+namespace commloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string command;
+
+            string inputfile;
+
+            Console.WriteLine("Command to execute.");
+            command = Console.ReadLine();
+
+            Console.WriteLine("Input File to Use.");
+            inputfile = Console.ReadLine();
+
+            foreach (string line in File.ReadLines(inputfile))
+            {
+                
+            }
+            File.ReadLines(inputfile);
+
+
+
+        }
+    }
+}
