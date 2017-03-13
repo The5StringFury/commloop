8bd4b0bdfcbfc144334d64e2a57a312ace143fd1
NA\AIUD457 <aiud457@amway.com>
2017-03-13 14:52:06 -0400

    Added more error correction and exitcodes -100 means we caught an exception  0 means we ran with out issue positive numbers indicate number of failures probably one per each line in the inputfile.

--- commloop/Program.cs
+++ commloop/Program.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using System.Collections;
 using System.Diagnostics;
-using System.ComponentModel;
+using System.IO;
+using System.Text;
 
 namespace commloop
 {
@@ -15,6 +10,8 @@ namespace commloop
         public static string command { get; private set; }
         public static string arguments { get; private set; }
         public static string inputfile { get; private set; }
+        public static int exitcode { get; private set; }
+
 
         static void Main(string[] args)
         {
@@ -28,13 +25,13 @@ namespace commloop
                 inputfile = Console.ReadLine();
             }
             else
-            {
+            {                
                 command = args[0];
-                 arguments = args[1];
-                 inputfile = args[2];
+                arguments = args[1];
+                inputfile = args[2];
             }
-
-
+            //we should exit with a code of 0  if all goes as planned. 
+            exitcode = 0;
             try
             {
                 foreach (string line in File.ReadLines(inputfile))
@@ -45,25 +42,30 @@ namespace commloop
                     commandargs.AppendFormat(arguments, line.Split(','));
                     try
                     {
-                        Process.Start(command, commandargs.ToString());
+                        Process.Start(command, commandargs.ToString()).WaitForExit();                        
                     }
                     catch (Exception e)
                     {
                         if (!EventLog.SourceExists("commloop.exe")) EventLog.CreateEventSource("commloop.exe", "Application");
                         EventLog.WriteEntry("commloop.exe", "Failure To Execute Line: " + line + " /n Exception: " + e, EventLogEntryType.Error);
+                        //The exit code should count the number of failures we got from running our command
+                        exitcode++;
                         continue;
-
                     }
-
-                }
-                
+                }                
             }
             catch (Exception x)
             {
                 Console.WriteLine(x);
+                exitcode = -100;
+                          
+            }  
+            finally
+            {
+                Console.WriteLine("commloop complete press any key...");
                 Console.ReadKey(false);
-                
-            }          
+                Environment.ExitCode = exitcode;
+            }        
             
         }
     }
